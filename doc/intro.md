5e388febbfe82f3d63af49dc1d5342f1b32b59ca
Stephen Hopper <hopper@deepmachine.com>
2017-03-07 09:11:18 -0600

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simply-credit-mailer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simply_credit_mailer/core_test.clj
@@ -0,0 +1,7 @@
+(ns simply-credit-mailer.core-test
+  (:require [clojure.test :refer :all]
+            [simply-credit-mailer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
